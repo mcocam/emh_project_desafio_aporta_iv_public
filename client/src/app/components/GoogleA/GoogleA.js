ed826b952cde8d79108a6902030fa842d219d23d
mcocam <38522390+mcocam@users.noreply.github.com>
2023-10-17 21:40:49 +0200

    + Update

--- client/src/app/components/GoogleA/GoogleA.js
+++ client/src/app/components/GoogleA/GoogleA.js
@@ -1,15 +1,14 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 export const GoogleA = () => {
     const location = useLocation();
 
     useEffect(() => {
         ReactGA.initialize("G-S5QFTRLRN1");
-        //ReactGA.pageview(location.pathname + location.search);
         
-        ReactGA.pageview(location.pathname + location.search);
         //ReactGA.pageview(location.pathname + location.search);
+        ReactGA.send({ hitType: "pageview", page: location.pathname , title: location.pathname });
     }, [location]);
 };
