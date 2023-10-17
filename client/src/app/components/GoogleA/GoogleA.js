b836239cd9eacf33817ff3c1b87befdd355f7c65
47901243V <47901243V>
2023-10-17 09:04:11 +0200

    + Analytics

--- client/src/app/components/GoogleA/GoogleA.js
+++ client/src/app/components/GoogleA/GoogleA.js
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 export const GoogleA = () => {
     const location = useLocation();
 
     useEffect(() => {
-        ReactGA.initialize("UA-243910965-1");
-        ReactGA.pageview(location.pathname + location.search);
+        ReactGA.initialize("G-S5QFTRLRN1");
+        //ReactGA.pageview(location.pathname + location.search);
+        
+        ReactGA.send({ hitType: "pageview", page: location.pathname + location.search, title: "Visits" });
+        //ReactGA.pageview(location.pathname + location.search);
     }, [location]);
 };
